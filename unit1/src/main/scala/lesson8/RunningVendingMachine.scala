3f263a2545df0cf24c1696c452342ee5c42cc764
Daniela Sfregola <daniela.sfregola@gmail.com>
2018-03-08 15:36:54 +0000

    [unit 1] reviewed code lesson 8

--- unit1/src/main/scala/lesson8/RunningVendingMachine.scala
+++ unit1/src/main/scala/lesson8/RunningVendingMachine.scala
@@ -1,18 +1,19 @@
 package lesson8
 
-object RunningVendingMachine extends App {
+object RunningVendingMachine {
 
-  val machine = new VendingMachine
-  println(machine.buy("chocolate", 1.5))
+  def main(args: Array[String]): Unit = {
+    val machine = new VendingMachine
 
-  val operator = new Operator(machine)
-  println(operator.refillProducts(2, 1))
+    println(machine.buy("chocolate", 1.5))
+    println(machine.buy("granola", 2))
 
-  machine.buy("chocolate", 1.5)
-  machine.buy("chocolate", 1)
-  machine.buy("granola", 2)
-  machine.buy("granola", 2)
+    machine.granolaBar = 10
+    machine.chocolateBar = 2
 
-  operator.collectMoney
+    println(machine.buy("chocolate", 1))
+    println(machine.buy("granola", 2))
 
+    println(s"Total money collected: ${machine.totalMoney} USD")
+  }
 }
