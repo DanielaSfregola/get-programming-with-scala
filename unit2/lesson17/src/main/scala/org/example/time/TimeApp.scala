275a73265149fab7c8394e0eac023d0bd31279f8
Daniela Sfregola <daniela.sfregola@gmail.com>
2018-04-30 10:54:58 +0100

    [unit 2] code lesson 17

--- unit2/lesson17/src/main/scala/org/example/time/TimeApp.scala
+++ unit2/lesson17/src/main/scala/org/example/time/TimeApp.scala
@@ -1,4 +1,4 @@
-package org.example.whattimeisit
+package org.example.time
 
 import cats.effect.IO
 import fs2.StreamApp
@@ -8,10 +8,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object TimeApp extends StreamApp[IO] {
 
-  def stream(args: List[String], requestShutdown: IO[Unit]) = BlazeBuilder[IO]
-  .bindHttp(8080, "0.0.0.0")
-  .mountService(timeService, "/")
-  .serve
+  def stream(args: List[String], requestShutdown: IO[Unit]) =
+    BlazeBuilder[IO]
+      .bindHttp(8080, "0.0.0.0")
+      .mountService(timeService, "/")
+      .serve
 
   def timeService = (new TimeService).service
 
