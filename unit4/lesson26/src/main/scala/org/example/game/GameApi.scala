3c66b8f76ebec135a9a17ceeb0135bdd0e718dc7
Daniela Sfregola <daniela.sfregola@gmail.com>
2020-12-03 19:31:02 +0000

    Upgraded http4s to 0.21.9

--- unit4/lesson26/src/main/scala/org/example/game/GameApi.scala
+++ unit4/lesson26/src/main/scala/org/example/game/GameApi.scala
@@ -2,12 +2,12 @@ package org.example.game
 
 import cats.effect.IO
 import org.example.game.entities.Game
-import org.http4s.HttpService
+import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
-class GameService extends Http4sDsl[IO] {
+class GameApi extends Http4sDsl[IO] {
 
-  val service = HttpService[IO] {
+  val routes = HttpRoutes.of[IO] {
     case req @ POST -> Root / "play" =>
       for {
         text <- req.as[String]
